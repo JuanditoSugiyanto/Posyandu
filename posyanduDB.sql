d9db58ec64a3bc8b9379a3d30899d33616330d78
JoenathanT <91514437+JoenathanT@users.noreply.github.com>
2024-06-26 07:42:20 +0700

    Add files via upload
    database v.4

--- posyanduDB.sql
+++ posyanduDB.sql
@@ -1,90 +1,102 @@
-﻿-- Buat database
-CREATE DATABASE posyandu;
-GO
-
--- Gunakan database
-USE posyandu;
-GO
-
--- Tabel balita
+﻿-- Tabel balita
 CREATE TABLE balita (
-    idAnak VARCHAR(255) PRIMARY KEY NOT NULL,
-    namaOrangtua VARCHAR(255) NOT NULL,
-    namaAnak VARCHAR(255) NOT NULL,
+    NIK VARCHAR(16) PRIMARY KEY,
+    FOREIGN KEY (namaOrangtua) REFERENCES orangtua(namaOrangtua),
+    namaAnak VARCHAR(100) NOT NULL,
     tanggalLahir DATE NOT NULL,
     umur INT NOT NULL,
-    jenisKelamin VARCHAR(10) NOT NULL,
+    jenisKelamin VARCHAR(10) CHECK (jenisKelamin IN ('laki-laki', 'perempuan')),
     alamat VARCHAR(255) NOT NULL,
-    namaPosyandu VARCHAR(255) NOT NULL,
-    beratBadan INT NOT NULL,
-    tinggiBadan INT NOT NULL,
+    namaPosyandu VARCHAR(255),
+    beratBadan INT,
+    tinggiBadan INT, 
     statusGizi VARCHAR(255)
 );
-GO
+
 
 -- Tabel userAccount
 CREATE TABLE userAccount (
-    NIK VARCHAR(16) PRIMARY KEY NOT NULL,
-    role VARCHAR(10) NOT NULL CHECK (role IN ('orangtua', 'kader')),
-    password VARCHAR(255) NOT NULL
+    NIK VARCHAR(16) PRIMARY KEY,
+    peran VARCHAR(10) CHECK (peran IN ('orangtua', 'kader')),
+    kataSandi VARCHAR(255) NOT NULL
 );
-GO
+
 
 -- Tabel orangtua
 CREATE TABLE orangtua (
-    idOrangtua VARCHAR(255) PRIMARY KEY NOT NULL,
-    NIK VARCHAR(16) NOT NULL,
-    idAnak VARCHAR(255) NOT NULL,
-    namaOrangtua VARCHAR(100) NOT NULL,
-    noTelp VARCHAR(13) NOT NULL,
+    NIK VARCHAR (16) PRIMARY KEY NOT NULL,
     FOREIGN KEY (NIK) REFERENCES userAccount(NIK),
-    FOREIGN KEY (idAnak) REFERENCES balita(idAnak)
+    NIK VARCHAR (16) PRIMARY KEY,
+    FOREIGN KEY (NIK) REFERENCES balita(NIK),
+    namaOrangtua VARCHAR(100) NOT NULL,
+    noTelp VARCHAR(13),  
 );
-GO
+
 
 -- Tabel petugas
 CREATE TABLE petugas (
     NIK VARCHAR(16) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (NIK) REFERENCES userAccount(NIK),
     namaPetugas VARCHAR(255) NOT NULL,
-    noTelp VARCHAR(13) NOT NULL CHECK (noTelp NOT LIKE '%[^0-9]%'),
-    peran VARCHAR(25) NOT NULL,
-    FOREIGN KEY (NIK) REFERENCES userAccount(NIK)
+    noTelp VARCHAR(13) CHECK (noTelp NOT LIKE '%[^0-9]%'),
+    peran VARCHAR(25),
 );
-GO
+
+CREATE TABLE RecordTimbangPersonal (
+    NIK INT PRIMARY KEY,
+    FOREIGN KEY (NIK) REFERENCES balita(NIK),
+    namaAnak VARCHAR(255) NOT NULL,
+    FOREIGN KEY (namaAnak) REFERENCES balita(namaAnak),
+    Tanggal_Timbang DATE NOT NULL,
+    Berat_Badan INT NOT NULL,
+    Tinggi_Badan INT NOT NULL,
+    Status_Gizi VARCHAR(255) NOT NULL
+);
+
+
 
 -- Tabel penerimaObatCacing
 CREATE TABLE penerimaObatCacing (
-    idAnak VARCHAR(255) PRIMARY KEY NOT NULL,
-    pemberianAbendazole BIT NOT NULL,
-    FOREIGN KEY (idAnak) REFERENCES balita(idAnak)
+    NIK VARCHAR(16) PRIMARY KEY,
+    FOREIGN KEY (NIK) REFERENCES balita(NIK),
+    namaAnak VARCHAR(100) NOT NULL,
+    FOREIGN KEY (namaAnak) REFERENCES balita(namaAnak),
+    PRIMARY KEY (ID_Anak, Nama_Anak),
+    pemberianAbendazole BIT NOT NULL
 );
-GO
+
 
 -- Tabel polaMakan
 CREATE TABLE polaMakan (
-    idAnak VARCHAR(255) PRIMARY KEY NOT NULL,
+    NIK VARCHAR(16) PRIMARY KEY,
+    FOREIGN KEY (NIK) REFERENCES balita(NIK),
+    namaAnak VARCHAR (100),
+    FOREIGN KEY (namaAnak) REFERENCES balita(namaAnak),
     daftarMakan VARCHAR(100) NOT NULL,
     intervensiGizi VARCHAR(100) NOT NULL,
     teksturMakanan VARCHAR(100) NOT NULL,
-    frekuensiMakan VARCHAR(100) NOT NULL,
-    FOREIGN KEY (idAnak) REFERENCES balita(idAnak)
+    frekuensiMakan VARCHAR(100) NOT NULL
 );
-GO
+
 
 -- Tabel kebersihanGigiAnak
 CREATE TABLE kebersihanGigiAnak (
-    idAnak VARCHAR(255) PRIMARY KEY NOT NULL,
+    NIK VARCHAR(16) PRIMARY KEY,
+    FOREIGN KEY (NIK) REFERENCES balita(NIK),
+    namaAnak VARCHAR (100),
+    FOREIGN KEY (namaAnak) REFERENCES balita(namaAnak),
+    jenisKelamin VARCHAR(10) CHECK (jenisKelamin IN ('laki-laki', 'perempuan')),
+    FOREIGN KEY (jenisKelamin) REFERENCES balita(jenisKelamin),
     tanggalPeriksa DATE NOT NULL,
     kebersihanGigi BIT,
-    karies BIT,
-    FOREIGN KEY (idAnak) REFERENCES balita(idAnak)
+    karies BIT
 );
-GO
+
 
 -- Tabel pemberianVitaminA
 CREATE TABLE pemberianVitaminA (
-    idAnak VARCHAR(255) PRIMARY KEY NOT NULL,
-    jenisVitaminA VARCHAR(5) NOT NULL CHECK (jenisVitaminA IN ('BIRU', 'MERAH')),
-    FOREIGN KEY (idAnak) REFERENCES balita(idAnak)
+    NIK VARCHAR(16) PRIMARY KEY NOT NULL,
+    FOREIGN KEY (NIK) REFERENCES balita(NIK),
+    jenisVitaminA VARCHAR(5) NOT NULL CHECK (jenisVitaminA IN ('BIRU', 'MERAH'))
 );
-GO
+
